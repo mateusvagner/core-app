2fffe9aa2883424296fccc6b941f6ac6e2a5b682
Mateus Vagner <mateus.vagner@gmail.com>
2023-09-01 12:13:52 -0300

    build: change compileSdk and targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.mv.coreapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mv.coreapp"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
