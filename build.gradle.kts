226ba956b85f134d7781a7b0e22e44368b7397c0
Mateus Vagner <mateus.vagner@gmail.com>
2023-09-08 16:55:47 -0300

    build: add Firebase Realtime Database into the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
     alias(libs.plugins.com.google.dagger.hilt.android) apply false
 
     alias(libs.plugins.org.jlleitschuh.gradle.ktlint) apply false
+
+    alias(libs.plugins.com.google.gms.google.services) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
 
