2e4e3db88fc47273ee287b652ddb0a8cf9f963fd
Mateus Vagner <mateus.vagner@gmail.com>
2023-09-08 16:58:03 -0300

    refactor: add firebase as dependency into datasource

--- app/src/main/java/com/mv/coreapp/data/datasource/remote/StudentRemoteDataSource.kt
+++ app/src/main/java/com/mv/coreapp/data/datasource/remote/StudentRemoteDataSource.kt
@@ -1,10 +1,16 @@
 package com.mv.coreapp.data.datasource.remote
 
+import com.google.firebase.database.FirebaseDatabase
 import com.mv.coreapp.data.datasource.StudentDataSource
 import com.mv.coreapp.network.dto.StudentDto
 import javax.inject.Inject
 
-class StudentRemoteDataSource @Inject constructor() : StudentDataSource {
+class StudentRemoteDataSource @Inject constructor(
+    private val firebaseDatabase: FirebaseDatabase
+) : StudentDataSource {
+
+    val databaseChild = firebaseDatabase.reference.child("student")
+
     override suspend fun saveStudent(student: StudentDto): Boolean {
         TODO("Not yet implemented")
     }
---
+++ app/src/main/java/com/mv/coreapp/di/FirebaseModule.kt
@@ -0,0 +1,18 @@
+package com.mv.coreapp.di
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Singleton
+    @Provides
+    fun provideFirebaseDatabase(): DatabaseReference = Firebase.database.reference
+}
