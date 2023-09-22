16aad647b6b12529ee70a47b55d44d5446f28d5f
Mateus Vagner <mateus.vagner@poatek.com>
2023-09-22 16:06:17 -0300

    build: add lib for navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,14 +60,15 @@ dependencies {
     implementation(libs.lifecycle.runtime.ktx)
 
     implementation(libs.activity.compose)
+    implementation(libs.navigation.compose)
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
 
     implementation(libs.material3)
-    testImplementation(libs.junit)
 
+    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(platform(libs.compose.bom))
