ee177e28852954245f81cdc044e6ca65cdc545b9
Mateus Vagner <mateus.vagner@gmail.com>
2023-10-12 12:45:51 -0300

    feat: create a custom bottom nav bar

--- app/src/main/java/com/mv/coreapp/navigation/CoreAppBottomNavigation.kt
+++ app/src/main/java/com/mv/coreapp/navigation/CoreAppBottomNavigation.kt
@@ -1,54 +1,69 @@
 package com.mv.coreapp.navigation
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mv.coreapp.designsystem.theme.CoreAppTheme
+import com.mv.coreapp.designsystem.theme.light_primarySurface
 
 @Composable
 fun CoreAppBottomNavigation(currentRoute: String, onClick: (String) -> Unit) {
-    NavigationBar(
+    Box(
         modifier = Modifier
-            .padding(horizontal = 8.dp).padding(bottom = 8.dp)
+            .padding(horizontal = 8.dp)
+            .padding(bottom = 8.dp)
             .background(
-                color = MaterialTheme.colorScheme.background,
-                shape = RoundedCornerShape(16.dp)
+                color = light_primarySurface,
+                shape = RoundedCornerShape(8.dp)
             )
-            .clip(RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(8.dp))
+            .fillMaxWidth()
     ) {
-        mainNavigationItems.forEach { navigationItem ->
-            NavigationBarItem(
-                icon = {
-                    Image(
-                        painter = painterResource(id = navigationItem.icon),
-                        contentDescription = stringResource(id = navigationItem.title)
-                    )
-                },
-                label = { Text(stringResource(navigationItem.title)) },
-                selected = navigationItem.route.value == currentRoute,
-                onClick = {
-                    onClick(navigationItem.route.value)
-                }
-            )
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            mainNavigationItems.forEach { navigationItem ->
+                CoreAppNavigationBarItem(
+                    modifier = Modifier.padding(8.dp),
+                    item = navigationItem,
+                    selected = navigationItem.route.value == currentRoute,
+                    onClick = {
+                        onClick(navigationItem.route.value)
+                    }
+                )
+            }
         }
     }
 }
 
 @Composable
 @Preview
-fun CoreAppBottomNavigationPreview() {
+fun CoreAppBottomNavigationCalendarPreview() {
+    CoreAppTheme {
+        CoreAppBottomNavigation(
+            currentRoute = BottomNavigationItem.Calendar.route.value,
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+@Preview
+fun CoreAppBottomNavigationStudentPreview() {
     CoreAppTheme {
         CoreAppBottomNavigation(
             currentRoute = BottomNavigationItem.Students.route.value,
---
+++ app/src/main/java/com/mv/coreapp/navigation/CoreAppNavigationBarItem.kt
@@ -0,0 +1,93 @@
+package com.mv.coreapp.navigation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mv.coreapp.designsystem.theme.CoreAppTheme
+
+@Composable
+fun CoreAppNavigationBarItem(
+    modifier: Modifier = Modifier,
+    item: BottomNavigationItem,
+    selected: Boolean = false,
+    onClick: () -> Unit = {}
+) {
+    val background =
+        if (selected) {
+            MaterialTheme.colorScheme.surface
+        } else {
+            Color.Transparent
+        }
+
+    val contentColor =
+        if (selected) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onSurfaceVariant
+
+    Box(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(background)
+            .clickable(onClick = onClick)
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = item.icon),
+                contentDescription = stringResource(id = item.title),
+                tint = contentColor
+            )
+            AnimatedVisibility(visible = selected) {
+                Text(
+                    text = stringResource(id = item.title),
+                    color = contentColor,
+                    maxLines = 1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun CoreAppNavigationBarItemSelectedPreview() {
+    CoreAppTheme {
+        CoreAppNavigationBarItem(
+            modifier = Modifier.padding(8.dp),
+            item = BottomNavigationItem.Calendar,
+            selected = true,
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+@Preview
+fun CoreAppNavigationBarItemUnselectedPreview() {
+    CoreAppTheme {
+        CoreAppNavigationBarItem(
+            modifier = Modifier.padding(8.dp),
+            item = BottomNavigationItem.Calendar,
+            selected = false,
+            onClick = {}
+        )
+    }
+}
