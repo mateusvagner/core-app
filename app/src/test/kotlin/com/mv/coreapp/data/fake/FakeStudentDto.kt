447988b37a38f22174d36c95af598b639f162e0a
Mateus Vagner <mateus.vagner@gmail.com>
2023-11-05 14:56:02 -0300

    test: add first tests for student repository

---
+++ app/src/test/kotlin/com/mv/coreapp/data/fake/FakeStudentDto.kt
@@ -0,0 +1,50 @@
+package com.mv.coreapp.data.fake
+
+import com.mv.coreapp.data.remotedatasource.dto.StudentDto
+
+object FakeStudentDto {
+
+    fun makeStudentDto(): StudentDto {
+        return StudentDto(
+            id = "1",
+            name = "John",
+            surname = "Doe",
+            birthDate = "1990-01-01T00:00:00.000+03:00",
+            enrollmentDate = "2023-01-01T00:00:00.000+03:00",
+            paymentDueDate = "2023-01-01T00:00:00.000+03:00",
+            modality = "MODALITY",
+            plan = "QUARTERLY",
+            status = "ACTIVE",
+            paymentStatus = "PAID"
+        )
+    }
+
+    fun makeStudentsDto(): List<StudentDto> {
+        return listOf(
+            StudentDto(
+                id = "1",
+                name = "John",
+                surname = "Doe",
+                birthDate = "1990-01-01'T'00:00:00.000Z",
+                enrollmentDate = "2023-01-01'T'00:00:00.000+03:00",
+                paymentDueDate = "2023-01-01'T'00:00:00.000+03:00",
+                modality = "MODALITY",
+                plan = "QUARTERLY",
+                status = "ACTIVE",
+                paymentStatus = "PAID"
+            ),
+            StudentDto(
+                id = "2",
+                name = "Jane",
+                surname = "Doe",
+                birthDate = "1990-01-01'T'00:00:00.000Z",
+                enrollmentDate = "2023-01-01'T'00:00:00.000+03:00",
+                paymentDueDate = "2023-01-01'T'00:00:00.000+03:00",
+                modality = "MODALITY",
+                plan = "MONTHLY",
+                status = "INACTIVE",
+                paymentStatus = "PENDING"
+            )
+        )
+    }
+}
